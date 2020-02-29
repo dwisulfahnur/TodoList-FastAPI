426905fb70bd02b683aa801aca3ddac669b63022
Dwi Sulfahnur <dwi.sulfahnur@jejaring3.id>
2020-02-29 14:26:26 +0700

    test: update the conftest file

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,11 +1,10 @@
 import pytest
 from databases import Database
-# from sqlalchemy import create_engine
 from starlette.testclient import TestClient
 
 from src.db import DATABASE_URL
 from src.db import database
-from ..main import app
+from src.main import app
 
 test_database = Database(DATABASE_URL, force_rollback=True)
 
